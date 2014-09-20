268e1bc790097221cd9d2c4aada441aaccb45478
Dan Calacci <dan@dcalacci.net>
2014-09-20 14:19:39 -0400

    place list frontend progress, moved js deps around

--- app.py
+++ app.py
@@ -38,38 +38,34 @@ def update_itinerary(parseid):
             headers=update_headers, data=request.get_data())
     return jsonify(r.json())
 
+
 @app.route('/')
 def hello():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(debug=True)
-
-
 # test points
 
-test_itinerary = """
-[ {
-           'id' : 'osmapid1',
-           'name' : 'Test Place 1',
-           'lat' : 12.34,
-           'lon' : 56.78
-       },
-  {
-           'id' : 'osmapid',
-           'name' : 'Test Place 2',
-           'lat' : 13.34,
-           'lon' : 57.78
-       },
- {
-           'id' : 'osmapid',
-           'name' : 'Test Place 3',
-           'lat' : 14.34,
-           'lon' : 56.78
-       }]
-"""
+test_itinerary = [{'id': 'osmapid1',
+                   'name': 'Test Place 1',
+                   'lat': 12.34,
+                   'lon': 56.78},
+
+                  {'id': 'osmapid',
+                   'name': 'Test Place 2',
+                   'lat': 13.34,
+                   'lon': 57.78},
+
+                  {'id': 'osmapid',
+                   'name': 'Test Place 3',
+                   'lat': 14.34,
+                   'lon': 56.78}]
 
 
-@app.route("/test/get_itinerary")
+@app.route('/test_get_itinerary', methods=['GET'])
 def test_get_itinerary():
-    return jsonify(test_itinerary)
+    print jsonify({'res': test_itinerary})
+    return jsonify({'res': test_itinerary})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/place_list.js
@@ -0,0 +1,27 @@
+// js for place listing
+
+
+function updatePlaceList() {
+    placeList = $('#itinerary-list ul')
+    $.get('/test_get_itinerary',
+         function(data) {
+             console.log("received list of places:");
+             console.log(data);
+             var places = data['res'];
+             for(i in places) {
+                 console.log(places[i].name);
+                 placeList.append('<li>' + places[i].name + '</li>');
+             }
+         })
+}
+
+
+$(document).ready(function() {
+    updatePlaceList();
+
+    $("ul#place-list").sortable({
+        opacity: 0.6,
+        cursor: 'move'
+    });
+
+});
