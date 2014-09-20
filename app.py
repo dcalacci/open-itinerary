98f02510a56efba298f12de05356744fae270d3a
Dan Calacci <dan@dcalacci.net>
2014-09-20 12:57:29 -0400

    removed test points import

--- app.py
+++ app.py
@@ -7,3 +7,32 @@ def hello():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+# test points
+
+test_itinerary = """
+[ {
+           'id' : 'osmapid1',
+           'name' : 'Test Place 1',
+           'lat' : 12.34,
+           'lon' : 56.78
+       },
+  {
+           'id' : 'osmapid',
+           'name' : 'Test Place 2',
+           'lat' : 13.34,
+           'lon' : 57.78
+       },
+ {
+           'id' : 'osmapid',
+           'name' : 'Test Place 3',
+           'lat' : 14.34,
+           'lon' : 56.78
+       }]
+"""
+
+
+@app.route("/test/get_itinerary")
+def test_get_itinerary():
+    return jsonify(test_itinerary)
