ccd61434911bf101e48cb915a09c47be74c258fb
Dan Calacci <dan@dcalacci.net>
2014-09-20 12:28:03 -0400

    removed lesscss requirement, added initial front page styling

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
-from flaskext.lesscss import lesscss
 app = Flask(__name__)
-
-# less -> css
-lesscss(app)
 
 @app.route("/")
 def hello():
     return render_template('index.html')
-
 
 if __name__ == "__main__":
     app.run(debug=True)
