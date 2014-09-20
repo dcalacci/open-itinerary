9d7d90e42d0669cb5f42140682833181d941ab1e
Dan Calacci <dan@dcalacci.net>
2014-09-20 11:45:17 -0400

    basic front-end template running

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
