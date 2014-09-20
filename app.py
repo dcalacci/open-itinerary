160b86ec1243ae3f6683486482224588163bdc7e
Dan Calacci <dan@dcalacci.net>
2014-09-20 16:07:39 -0400

    Merged app.py, updated to (mostly) PEP-8

--- app.py
+++ app.py
@@ -5,38 +5,53 @@ import requests
 app = Flask(__name__)
 app.config.from_object('config')
 
-# returns: 
-# { "createdAt": "2014-09-20T18:13:06.612Z", "objectId": "damL3gBx0e" }
+
 @app.route('/itinerary/create', methods=['POST'])
 def create_itinerary():
+    """Creates an itinerary with the given JSON itinerary data.
+
+    returns:
+        { "createdAt": "2014-09-20T18:13:06.612Z", "objectId": "damL3gBx0e" }
+    """
     r = requests.post('https://api.parse.com/1/classes/Itinerary',
-            headers=app.config['PARSE_HEADERS'], data=request.get_data())
+                      headers=app.config['PARSE_HEADERS'],
+                      data=request.get_data())
     return jsonify(r.json())
 
-# returns:
-# { "itinerary": [ { "id": "abcdefg", "lat": 12.34, "lon": 34.56, "name": "test" } ] }
+
 @app.route('/itinerary/<parseid>', methods=['GET'])
 def get_itinerary(parseid):
-    r = requests.get('https://api.parse.com/1/classes/Itinerary/{}'.format(parseid),
-            headers=app.config['PARSE_HEADERS'])
-    return render_template('index.html', itinerary=jsonify(itinerary=r.json()['itinerary']))
+    """Gets the itinerary for a given parseid.
 
-# requires JSON to be posted in the following format:
-'''
-{ "itinerary": [ {
-            "id": "abcdefg",
-            "name": "test",
-            "lat": 12.34,
-            "lon": 34.56
-        } ] }
-''' 
-# returns: { "updatedAt": "2014-09-20T17:57:36.171Z" }
+    returns:
+        { "itinerary": [ { "id": "abcdefg", "lat": 12.34, "lon": 34.56, "name": "test" } ] }
+    """
+    r = requests.get('https://api.parse.com/1/classes/Itinerary/{}'.format(parseid),
+                     headers=app.config['PARSE_HEADERS'])
+    return render_template('index.html',
+                           itinerary=jsonify(itinerary=r.json()['itinerary']))
+
+
 @app.route('/itinerary/<parseid>', methods=['POST'])
 def update_itinerary(parseid):
-    json_header = {'Content-Type': 'application/json'} 
-    update_headers = dict(app.config['PARSE_HEADERS'].items() + json_header.items()) 
-    r = requests.put('https://api.parse.com/1/classes/Itinerary/{}'.format(parseid), 
-            headers=update_headers, data=request.get_data())
+    """ Gets the itinerary for a particular parseid
+
+    POST data must follow this format:
+
+        { "itinerary": [ {
+                "id": "abcdefg",
+                "name": "test",
+                "lat": 12.34,
+                "lon": 34.56
+         }, ] }
+
+    returns:
+        { "updatedAt": "2014-09-20T17:57:36.171Z" }
+    """
+    json_header = {'Content-Type': 'application/json'}
+    update_headers = dict(app.config['PARSE_HEADERS'].items() + json_header.items())
+    r = requests.put('https://api.parse.com/1/classes/Itinerary/{}'.format(parseid),
+                     headers=update_headers, data=request.get_data())
     return jsonify(r.json())
 
 
@@ -44,32 +59,11 @@ def update_itinerary(parseid):
 def hello():
     return render_template('index.html')
 
-# test points
-
-test_itinerary = [{'id': 'osmapid1',
-                   'name': 'Test Place 1',
-                   'lat': 12.34,
-                   'lon': 56.78},
-
-                  {'id': 'osmapid',
-                   'name': 'Test Place 2',
-                   'lat': 13.34,
-                   'lon': 57.78},
-
-                  {'id': 'osmapid',
-                   'name': 'Test Place 3',
-                   'lat': 14.34,
-                   'lon': 56.78}]
-
-
-@app.route('/test_get_itinerary', methods=['GET'])
-def test_get_itinerary():
-    print jsonify({'res': test_itinerary})
-    return jsonify({'res': test_itinerary})
 
 @app.route("/homepage")
 def go():
     return render_template('homepage.html')
+
 
 @app.route("/location/<zipcode>/<addresses>")
 def loc(zipcode, addresses):
--- static/js/place_list.js
+++ static/js/place_list.js
@@ -1,24 +1,36 @@
 // js for place listing
 
 
-function updatePlaceList() {
+// popup DOM for a particular place. HTML with name, description, etc.
+function popupForPlace(place) {
+  "<span id='place-name'>" + place['name'] + "</span><br/>"
+}
+
+// adds a given place to the leaflet map as a marker
+// TODO: give it an alphabetical or numerical ID
+function addPlaceToMap(place) {
+    L.marker([place['lat'], place['lon']]).addTo(map).bindPopup(popupForPlace(place))
+}
+
+// updates the place list for the given parseid
+function updatePlaceList(parseid) {
     placeList = $('ul#place-list')
-    $.get('/test_get_itinerary',
-         function(data) {
-             console.log("received list of places:");
-             console.log(data);
-             var places = data['res'];
-             for(i in places) {
-                 console.log(places[i].name);
-                 placeList.append("<li class='place-item'>" + places[i].name + '</li>');
-             }
-         })
+    $.get('/itinerary/' + parseid,
+          function(data) {
+              console.log("received list of places:");
+              console.log(data);
+              var places = data['itinerary'];
+              for(i in places) {
+                  console.log(places[i].name);
+                  placeList.append("<li class='place-item'>" + places[i].name + '</li>');
+                  addPlaceToMap(places[i]);
+              }
+          });
 }
 
 
 $(document).ready(function() {
-    updatePlaceList();
-
+    updatePlaceList('Mw3O68vpF8');
     $("ul#place-list").sortable({
         opacity: 0.6,
         cursor: 'move'
