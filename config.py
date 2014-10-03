6ffc7de03303dca07b11381c488176b891a06177
Dan Calacci <dan@dcalacci.net>
2014-10-03 16:11:15 -0400

    updated config to read keys correctly

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ DEBUG = True
 
 # required headers when making parse requests
 with open('/var/www/keys/open-itinerary.txt', 'rb') as f:
-    keys = f.readlines()
+    keys = [key.strip() for key in f.readlines()]
 
 PARSE_HEADERS = {
-        'X-Parse-Application-Id' : keys[0]
-        'X-Parse-REST-API-Key' : keys[1]
+        'X-Parse-Application-Id': keys[0],
+        'X-Parse-REST-API-Key': keys[1]
         }
